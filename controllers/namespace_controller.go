187d141a2d581012a39e4ac258d35ecab8683545
Mohammad Yosefpor <myusefpur@gmail.com>
2022-02-28 01:48:49 +0330

    fix: add rbac

--- controllers/namespace_controller.go
+++ controllers/namespace_controller.go
@@ -46,6 +46,8 @@ type NamespaceReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=namespaces/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=core,resources=namespaces/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=integreatly.org,resources=grafanadatasources,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
