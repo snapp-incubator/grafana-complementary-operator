1b03875a5fd00c30c475f8e2fde83bdc84f60849
Mohammad Yosefpor <myusefpur@gmail.com>
2022-02-28 04:03:07 +0330

    fix: add grafana crd to scheme

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	grafanav1alpha1 "github.com/grafana-operator/grafana-operator/v4/api/integreatly/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -43,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(grafanav1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
